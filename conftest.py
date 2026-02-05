2873269eb9680638645455242301edb23a40c07b
Rahul <rahulpoojari120@gmail.com>
2026-02-05 17:24:38 +0530

    fix CI headless mode

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)# fixed
         context = browser.new_context()
         page = context.new_page()
 
