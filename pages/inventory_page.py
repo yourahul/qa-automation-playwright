44fc535f5516d5acfceaec28243567ad5800e7bb
Rahul <rahulpoojari120@gmail.com>
2026-02-06 09:35:45 +0530

    Final working QA automation framework

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,12 +1,9 @@
 class InventoryPage:
-
     def __init__(self, page):
         self.page = page
-        self.add_to_cart_btn = "#add-to-cart-sauce-labs-backpack"
-        self.cart_icon = ".shopping_cart_link"
 
     def add_item_to_cart(self):
-        self.page.click(self.add_to_cart_btn)
+        self.page.click("button[id='add-to-cart-sauce-labs-backpack']")
 
-    def open_cart(self):
-        self.page.click(self.cart_icon)
+    def get_cart_count(self):
+        return self.page.locator(".shopping_cart_badge").inner_text()
